7ff5427c17b862386ec505c68cb88bd389aaa169
Victormarzo <victor.marzo.monteiro@gmail.com>
2022-11-23 11:03:36 -0300

    test

---
+++ src/routers/hotels-router.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { authenticateToken } from "@/middlewares";
+
+const hotelsRouter = Router();
+
+hotelsRouter
+  .all("/*", authenticateToken);
+ 
+export { hotelsRouter };
